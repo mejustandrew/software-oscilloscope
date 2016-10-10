85424705e83f98955ccb436c6d7bdd7eb0ba32e5
mihaivma <none@none>
2016-10-10 20:23:58 +0300

    included the new test class.
    --HG--
    branch : UnitTesting

---
+++ DataAcquisitionTests/DataResponseTest.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "DataResponse.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DataAcquisitionTests
+{		
+	TEST_CLASS(DataResponseTest)
+	{
+	public:
+
+		TEST_METHOD(CircularBuffer_ShuldReturnConvrtedValues_InSameOrder)
+		{
+			int number_of_values=10;
+			double conversion_coefficient= ( 1 << 16 )-1;
+			DataResponse *response = new DataResponse(); 
+			for(double i=0;i<number_of_values;++i)
+			{
+				response->Add(i);
+			}
+
+			for(double i=0;i<number_of_values;++i)
+			{
+				Assert::AreEqual(i/conversion_coefficient,(*response)[i]);
+			}
+		}
+	};
+}
