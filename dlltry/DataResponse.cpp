7ae76746029fbac9af947ff28cba9124274f850f
mihaivma <none@none>
2017-02-13 13:56:39 +0200

    Fixed unit tests and DataResponse bug which stepped over one sample.

--- dlltry/DataResponse.cpp
+++ dlltry/DataResponse.cpp
@@ -1,56 +1,54 @@
 #include"DataResponse.h"
 
-int DataResponse::size(){	return loop_size; }
+int DataResponse::size() { return loop_size; }
 
-double DataResponse::operator[] ( int index ){	return samples[(index+access_pos)%number_of_samples]; }
+double DataResponse::operator[] (int index)
+{
+	return samples[(index + access_pos) % number_of_samples];
+}
 
 void DataResponse::Destroy()
 {
-	over_buffer=false;
-	access_pos=0;
-	add_pos=0;
+	over_buffer = false;
+	access_pos = 0;
+	add_pos = 0;
 }
 
 void DataResponse::Add(double sample)
 {
-	samples[add_pos]=sample/conversion_coefficient;
+	samples[add_pos] = sample / conversion_coefficient;
 	++add_pos;
-	if(add_pos==number_of_samples)
+	if (add_pos == number_of_samples)
 	{
-		add_pos=0;
-		over_buffer=true;
+		add_pos = 0;
+		over_buffer = true;
 	}
 }
 
 bool DataResponse::SetLoopSize(int i)
 {
-	if(over_buffer)
+	loop_size = i;
+	if (over_buffer)
 	{
-		loop_size = i;
-		access_pos = add_pos+1;
+		access_pos = add_pos;
 		return true;
 	}
-	if(i < add_pos)
-	{
-		loop_size = i;
-		return true;	
-	}
-	return false;
+	return i < add_pos;
 }
 
-bool DataResponse::SetSamples(int number,double treshold)
+bool DataResponse::SetSamples(int number, double treshold)
 {
-	int i=0;
-	double converted_treshold=treshold;
-	if(over_buffer)
+	int i = 0;
+	double converted_treshold = treshold;
+	if (over_buffer)
 	{
-		i=add_pos+1;
-		while(number+i<number_of_samples)
+		i = add_pos + 1;
+		while (number + i < number_of_samples)
 		{
-			if(converted_treshold>samples[i%number_of_samples] && converted_treshold<=samples[(i+1)%number_of_samples])
+			if (converted_treshold > samples[i%number_of_samples] && converted_treshold <= samples[(i + 1) % number_of_samples])
 			{
-				access_pos=i;
-				loop_size=number;
+				access_pos = i;
+				loop_size = number;
 				return true;
 			}
 			++i;
@@ -59,12 +57,12 @@ bool DataResponse::SetSamples(int number,double treshold)
 	}
 	else
 	{
-		while(number+i<add_pos)
+		while (number + i < add_pos)
 		{
-			if(converted_treshold>samples[i] && converted_treshold<=samples[i+1])
+			if (converted_treshold > samples[i] && converted_treshold <= samples[i + 1])
 			{
-				access_pos=i;
-				loop_size=number;
+				access_pos = i;
+				loop_size = number;
 				return true;
 			}
 			++i;
--- dlltry/DataResponse.h
+++ dlltry/DataResponse.h
@@ -1,33 +1,33 @@
 #pragma once
 #include"IDataResponse.h"
 
- class _declspec(dllexport) DataResponse: public IDataResponse
+class _declspec(dllexport) DataResponse : public IDataResponse
 {
-	double*samples,conversion_coefficient;
+	double*samples, conversion_coefficient;
 	int number_of_samples,//used to initialize samples vector
 		add_pos,//to add on it, and it's the last position for added sample
 		access_pos,//used when samples are read with operator
 		loop_size;//used to specify the size of iterations
 	bool over_buffer;
 public:
-	DataResponse()
+	DataResponse(int number_of_samples = 64000) : number_of_samples(number_of_samples)
 	{
-		over_buffer=false;
-		loop_size=0;
-		access_pos=0;
-		add_pos=0;
-		number_of_samples=64000;
-		samples=new double[number_of_samples];
-		conversion_coefficient= ( 1 << 16 )-1;
-		for(int i=0;i<number_of_samples;++i)
+		over_buffer = false;
+		loop_size = 0;
+		access_pos = 0;
+		add_pos = 0;
+		number_of_samples = 64000;
+		samples = new double[number_of_samples];
+		conversion_coefficient = (1 << 16) - 1;
+		for (int i = 0; i < number_of_samples; ++i)
 		{
-			samples[i]=0;
+			samples[i] = 0;
 		}
 	}
-	 int size();
-	 double operator[] ( int index );
-	 void Destroy();
-	 void Add(double sample);
-	 bool SetSamples(int number,double treshold);
-	 bool SetLoopSize(int i);
+	int size();
+	double operator[] (int index);
+	void Destroy();
+	void Add(double sample);
+	bool SetSamples(int number, double treshold);
+	bool SetLoopSize(int i);
 };
