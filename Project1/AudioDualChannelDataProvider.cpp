c348612bd3d892a1240396b9bf36e12f9b860e3b
mihaivma <none@none>
2017-05-28 11:31:54 +0300

    Removed data aquisition countinuos call until receiving an answer.

--- Project1/AudioDualChannelDataProvider.cpp
+++ Project1/AudioDualChannelDataProvider.cpp
@@ -8,13 +8,8 @@ DataContainer * AudioDualChannelDataProvider::GetNewData(IDataRequest * leftChan
 	if (!WorkingFrame::IsAudioStreamInitialized())
 		return container;
 
-	while (true)
-	{
-		container->LeftChannelData = GetSignalLeftSamples(leftChannelRequest);
-		container->RightChannelData = GetSignalRightSamples(rightChannelRequest);
+	container->LeftChannelData = GetSignalLeftSamples(leftChannelRequest);
+	container->RightChannelData = GetSignalRightSamples(rightChannelRequest);
 
-		if (container->LeftChannelData || container->RightChannelData)
-			return container;
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
-	}
+	return container;
 }
